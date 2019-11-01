1ebe9f08216f90d1d1c24ace4406a6e8c8b1fd45
harry1pacman <55867944+harry1pacman@users.noreply.github.com>
2019-11-01 08:58:34 +0500

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,12 +1,5 @@
 a = int(input())
 b = int(input())
 c = int(input())
-if a < b and a < c:
-    print (a, 'меньшее число')
-elif b<a and b<c:
-    print (b, 'меньшее число')
-elif c<a and c<b:
-    print (c, 'меньшее число')
-else:
-    print ('числа равны')
-    
+print (min(a,b,c)) 
+print (max(a,b,c)) 
